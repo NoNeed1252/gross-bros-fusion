41f1a1891fb97074b7ec483ae61146f13f079065
NoNeed1252 <closecombatchimps@icloud.com>
2026-08-05 13:51:03 -0400

    Update arcade-tab.tsx

--- components/tabs/arcade-tab.tsx
+++ components/tabs/arcade-tab.tsx
@@ -6,12 +6,13 @@ import { Leaderboard } from '@/components/arcade/leaderboard'
 
 interface ArcadeTabProps {
   bro: GrossBro
+  walletAddress?: string
 }
 
-export function ArcadeTab({ bro }: ArcadeTabProps) {
+export function ArcadeTab({ bro, walletAddress }: ArcadeTabProps) {
   return (
     <div className="mx-auto w-full max-w-3xl space-y-2">
-      <InvadersGame bro={bro} />
+      <InvadersGame bro={bro} walletAddress={walletAddress} />
       <Leaderboard />
     </div>
   )
