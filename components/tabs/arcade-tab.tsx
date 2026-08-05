e974229ddc88edbee43d059b6221e3226b750a7c
NoNeed1252 <closecombatchimps@icloud.com>
2026-08-05 11:24:51 -0400

    Update arcade-tab.tsx

--- components/tabs/arcade-tab.tsx
+++ components/tabs/arcade-tab.tsx
@@ -1,1 +1,16 @@
-$(cat /workspace/user/arcade-tab.tsx)
+'use client'
+
+import type { GrossBro } from '@/lib/gross-bros'
+import { InvadersGame } from '@/components/arcade/invaders-game'
+
+interface ArcadeTabProps {
+  bro: GrossBro
+}
+
+export function ArcadeTab({ bro }: ArcadeTabProps) {
+  return (
+    <div className="mx-auto w-full max-w-3xl">
+      <InvadersGame bro={bro} />
+    </div>
+  )
+}
