abb3498d25b8113d2832bd911bbe53f6cf50cd0e
NoNeed1252 <closecombatchimps@icloud.com>
2026-08-05 11:50:14 -0400

    Update arcade-tab.tsx

--- components/tabs/arcade-tab.tsx
+++ components/tabs/arcade-tab.tsx
@@ -2,6 +2,7 @@
 
 import type { GrossBro } from '@/lib/gross-bros'
 import { InvadersGame } from '@/components/arcade/invaders-game'
+import { Leaderboard } from '@/components/arcade/leaderboard'
 
 interface ArcadeTabProps {
   bro: GrossBro
@@ -9,8 +10,9 @@ interface ArcadeTabProps {
 
 export function ArcadeTab({ bro }: ArcadeTabProps) {
   return (
-    <div className="mx-auto w-full max-w-3xl">
+    <div className="mx-auto w-full max-w-3xl space-y-2">
       <InvadersGame bro={bro} />
+      <Leaderboard />
     </div>
   )
 }
